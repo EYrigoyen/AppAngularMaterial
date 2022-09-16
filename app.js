b98b8f2c78552be1f9955f2999c6ec44c586a6d7
Eduardo Yrigoyen <eyrigoyen78@gmail.com>
2022-09-16 16:01:49 -0500

    conexion mysql

---
+++ app.js
@@ -0,0 +1,20 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Root@12345',
+    database : 'persona'
+});
+
+connection.connect();
+
+connection.connect(function (error) {
+    if (error) {
+        throw error;
+    }else {
+        console.log('CONEXIÓN EXITOSA');
+    }
+    
+});
+
+connection.end();
